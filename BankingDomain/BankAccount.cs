0c57b558a6faea3f6aa507e1ce4939cdc4e83582
coraw24@gmail.com <coraw24@gmail.com>
2020-09-15 12:30:40 -0600

    Done with this

--- BankingDomain/BankAccount.cs
+++ BankingDomain/BankAccount.cs
@@ -4,9 +4,11 @@ using System.Text;
 
 namespace BankingDomain
 {
+    public enum AccountType { Standard, Gold}
     public class BankAccount
     {
         private decimal _balance = 1000M;
+        public AccountType AccountType;
         public decimal GetBalance()
         {
             return _balance;
@@ -14,12 +16,25 @@ namespace BankingDomain
 
         public void Deposit(decimal amountToDeposit)
         {
+            switch (AccountType)
+            {
+                case AccountType.Standard:
+                    {
+                        _balance += amountToDeposit;
+                        break;
+                    }
+                case AccountType.Gold:
+                    {
+                        _balance += amountToDeposit * 1.10M;
+                        break;
+                    }
+            }
             _balance += amountToDeposit;
         }
 
         public void Withdraw(decimal amountToWithdraw)
         {
-            if(amountToWithdraw > _balance)
+            if (amountToWithdraw > _balance)
             {
                 throw new OverdraftException();
             }
---
+++ BankingUnitTest/GoldAccountDeposits.cs
@@ -0,0 +1,24 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingUnitTest
+{
+    public class GoldAccountDeposits
+    {
+        [Fact]
+        public void GoldAccountsGetABonusOnDeposits()
+        {
+            var account = new BankAccount();
+            account.AccountType = AccountType.Gold;
+            var openingBalance = account.GetBalance();
+            var amountToDeposit = 100M;
+
+            account.Deposit(amountToDeposit);
+
+            Assert.Equal(openingBalance + amountToDeposit + 10M, account.GetBalance());
+        }
+    }
+}
